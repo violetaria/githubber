32919e3e0d65d463b3d3b46c651b8f9d935108f1
violetaria <terri.chu@outlook.com>
2015-10-13 20:20:19 -0400

    updated with hard portion of homework

--- lib/githubber.rb
+++ lib/githubber.rb
@@ -5,6 +5,18 @@ require "githubber/version"
 require "githubber/issues"
 
 module Githubber
-  # Your code goes here...
+  puts "Enter your secret key"
+  key = gets.chomp
+
+  issue_api = Issues.new(key)
   binding.pry
+
+  issue_api.close_issue("TIY-ATL-ROR-2015-Sep",
+                        "assignments",
+                        201)
+
+  issue_api.comment_issue("TIY-ATL-ROR-2015-Sep",
+                          "assignments",
+                          201,
+                          {body: "pull request: https://github.com/violetaria/githubber/pull/2"})end
 end
